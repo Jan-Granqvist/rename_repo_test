d72a9a99d216cc9c35bc49a7e5ec28054c7d52f2
Janne Granqvist <jan.granqvist@teliacompany.com>
2024-05-08 08:12:18 +0200

    Modified main.cpp in branch feature/addinstuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <memory>
+
+class Helper
+{
+	public:
+		explicit Helper(const std::string input)
+		: m_string(std::move(input)) {}
+		~Helper()
+		{
+			std::cout << m_string << std::endl;
+		}
+	private:
+		std::string	m_string;
+};
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello world!!" << std::endl;
+	std::unique_ptr<Helper>	helper = std::make_unique<Helper>(std::move("Hello world from helper!!"));
 }
